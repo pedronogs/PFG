8aa3815aa4ac0f0783693b3c39e60dcc76e95652
Pedro "Nogs <37855212+pedronogs@users.noreply.github.com>
2021-09-17 19:23:26 -0300

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -6,4 +6,5 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf$ ~/.fzf/install
 cp .zshrc ~/.zshrc
