52fbc7a2b111ce514371aa44badb5e6b379c436f
Pedro "Nogs <37855212+pedronogs@users.noreply.github.com>
2021-09-17 19:18:27 -0300

    Initial script to setup ubuntu

---
+++ setup_ubuntu.sh
@@ -0,0 +1,9 @@
+# Install ZSH shell
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Install OH MY ZSH and plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+cp .zshrc ~/.zshrc
