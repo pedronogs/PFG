12db5c9c08602757aad86d85ebbea2a4cef2cd9d
pedronogs <phnogs@hotmail.com>
2021-09-17 21:30:38 -0400

    Update Ubuntu setup script, fixed user as osboxes

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,15 +1,18 @@
+# Install basic packages
+apt-get install -y vim git zsh curl
+
 # Configure GIT
-sudo apt install git
 git config --global user.name "pedronogs"
 git config --global user.email "phnogs@hotmail.com"
 
-# Install ZSH shell
-sudo apt install zsh
-chsh -s $(which zsh)
-
 # Install OH MY ZSH and plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf$ ~/.fzf/install
+#git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Configure ZSH shell
 cp .zshrc ~/.zshrc
+chsh -s $(which zsh) osboxes
